17973c2c7593041e28aaf697637d9aa30ed0f16d
Islam-Tarek <islamtarek258@gmail.com>
2025-06-11 19:39:26 +0300

    feat: add users module

---
+++ apps/api/src/users/dto/user.dto.ts
@@ -0,0 +1,12 @@
+export type UserRole = 'manager' | 'developer' | 'qc';
+
+export class UserDto {
+  id!: string;
+  username!: string;
+  displayName!: string;
+  email!: string;
+  workspaces!: string[];
+  role!: UserRole;
+  password!: string;
+  createdAt?: Date;
+}
