fcc0ead922bba0d5ffa45bffb75c0235cfded015
Islam-Tarek <islamtarek258@gmail.com>
2025-06-25 21:37:39 +0300

    add: - add RequestWorkspace interface to use it as type - add WorkspaceGuardOptions interface to use it as type

---
+++ apps/api/src/interfaces/workspace-guard-options.interface.ts
@@ -0,0 +1,5 @@
+export interface WorkspaceGuardOptions {
+  workspaceIdParamKey?: string; //  'id', 'workspaceId'
+  userIdParamKey?: string; //  'userId', 'memberId'
+  userIdBodyKey?: string; //  'userId', 'membernameOrEmail'
+}
