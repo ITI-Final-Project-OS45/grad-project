77bccb0a2524e8092c309dbdd9fcae7c21d83c77
Moamen_Alghareeb <moamenalghareeb10@gmail.com>
2025-07-11 21:33:19 +0300

    refactor: Clean up next.config.js formatting and remove experimental settings

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    esmExternals: "loose",
-  },
-  transpilePackages: ["@repo/types"],
+  transpilePackages: ['@repo/types'],
   webpack: (config) => {
     // Handle decorators in shared packages
     config.module.rules.push({
       test: /\.tsx?$/,
       include: /node_modules\/@repo\/types/,
       use: {
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
-          presets: [["@babel/preset-env", { targets: { node: "current" } }], "@babel/preset-typescript"],
+          presets: [
+            ['@babel/preset-env', { targets: { node: 'current' } }],
+            '@babel/preset-typescript',
+          ],
           plugins: [
-            ["@babel/plugin-proposal-decorators", { legacy: true }],
-            "@babel/plugin-transform-class-properties",
+            ['@babel/plugin-proposal-decorators', { legacy: true }],
+            '@babel/plugin-transform-class-properties',
           ],
         },
       },
